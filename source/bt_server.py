66aa5fc3b411e14add74c177aea3c6f94de88ea7
knguyen2133 <knguyen2133@gmail.com>
2019-09-19 20:11:20 -0500

    Update bt_server.py

--- source/bt_server.py
+++ source/bt_server.py
@@ -1,9 +1,3 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
-#
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
-
 from bluetooth import *
 import time, threading, socket, network_ip, Queue
 
